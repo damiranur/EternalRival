beedc97660bd72a79b80d3ca93073c19b5f75a80
Andrei Paleshka <andrepoleshko@gmail.com>
2023-10-29 16:19:36 +0100

    feat: implement types

---
+++ src/types/index.ts
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react';
+
+export interface ICharacter {
+  id: number;
+  name: string;
+  image: string;
+  species: string;
+  gender: string;
+}
+
+export interface IAppState {
+  searchTerm: string;
+  characters: ICharacter[];
+  isLoading: boolean;
+}
+
+export interface IMainProps {
+  characters: ICharacter[];
+  isLoading: boolean;
+}
+
+export interface ISearchTermProps {
+  setSearchTerm: (searchTerm: string) => void;
+}
+
+export type IChildrenProps = {
+  children?: ReactNode;
+};
+
+export interface IErrorBoundaryState {
+  hasError: boolean;
+}
+
+export type ApiCharactersData = { results: ICharacter[] };
+
+export interface ICharactersListProps {
+  characters: ICharacter[];
+}
+
+export type PlaceholderProps = Record<string, never>;
+
+export interface IErrorButtonState {
+  error: Error | null;
+}
