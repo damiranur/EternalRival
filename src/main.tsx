e334a6f24067e9fc7fd108c25b689be5df88fe09
Andrei Paleshka <andrepoleshko@gmail.com>
2023-11-05 12:17:00 +0100

    feat: add router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import ErrorBoundary from './components/ErrorBoundary/index.tsx';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
