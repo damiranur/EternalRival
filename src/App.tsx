1b9472cca89cb9bc9087a96447e09b9953e7d2fe
Andrei Paleshka <andrepoleshko@gmail.com>
2023-11-09 11:43:51 +0100

    feat: add app context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import MainPage from './pages/MainPage';
+import AppProvider from './context';
+import './App.module.scss';
 
 const App = () => {
-  return <MainPage />;
+  return (
+    <AppProvider>
+      <MainPage />
+    </AppProvider>
+  );
 };
 
 export default App;
