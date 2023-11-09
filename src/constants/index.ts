6bb8c461faba314e021e219ff7b63f0a5bb3c279
Andrei Paleshka <andrepoleshko@gmail.com>
2023-11-09 11:36:19 +0100

    feat: add constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,6 @@
 export const LOCAL_STORAGE_SEARCH_TERM = 'searchTerm';
+export const LOCAL_STORAGE_IS_OPEN = 'isOpen';
+
+export const DEFAULT_PER_PAGE = 6;
+export const DEFAULT_CURRENT_PAGE = 1;
+export const DEFAULT_TOTAL_PAGES = 1;
